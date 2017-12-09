2962eb71020026314b399e7e141f2dc79a77bb0c
zannah-rain <zoe.pellatt.0@gmail.com>
2017-12-09 20:43:39 +0000

    Added todo list

---
+++ doc/todo.R
@@ -0,0 +1,108 @@
+# REMOVE TRIBUTE NON-ALLY
+# Add initial strategic numbers to AI?
+# Not gathering stone because max-dropsite distance is 2!
+#   Could have more reasonable values in there?
+
+
+
+# Add these SNs
+# SNAttackGroupMakeup
+# SNChokePointDefendPriority
+# SNDefendGroupMakeup
+# SNExploreGroupMakeup
+# SNPercentVictoryClamp
+# SNScalePercentDeathRetreat
+# SNScalePercentHealthRetreat
+# SNScalePercentUnitHealthRetreat
+# SNSpecialAttackInfluence2
+# SNSpecialAttackInfluence3
+# SNSpecificBuildItemTime
+# SNSpecificBuildItemToBuild
+# SNTacticalUpdateFrequency
+# SNTributeAmount
+# SNTributeChatFrequency
+# SNTributeChatRandomness
+# SNTributePersistence
+# SNTributePlayer
+# SNTributeTimeout
+# SNUnskippableItemType
+# SNUpgradeToBronzeAgeASAP
+# SNUpgradeToIronAgeASAP
+# SNUpgradeToToolAgeASAP
+#
+#
+# sn-allow-civilian-defense
+# sn-attack-diplomacy-impact
+# sn-attack-group-gather-spacing
+# sn-attack-group-size-randomness
+# sn-attack-intelligence
+# sn-attack-separation-time-randomness
+# sn-attack-winning-player
+# sn-attack-winning-player-factor
+# sn-blot-size
+# sn-build-frequency
+# sn-camp-max-distance
+# sn-cap-civilian-explorers
+# sn-cap-civilian-gatherers
+# sn-consecutive-idle-unit-limit
+# sn-coop-share-attacking
+# sn-coop-share-attacking-interval
+# sn-coop-share-information
+# sn-defend-overlap-distance
+# sn-defense-distance
+# sn-dock-defend-priority
+# sn-do-not-scale-for-difficulty-level
+# sn-do-not-transport-from-same-zone
+# sn-easier-reaction-percentage
+# sn-easiest-reaction-percentage
+# sn-enemy-sighted-response-distance
+# sn-food-gatherer-percentage
+# sn-forage-defend-priority
+# sn-garrison-rams
+# sn-gather-defense-units
+# sn-gather-idle-soldiers-at-center
+# sn-gold-defend-priority
+# sn-gold-gatherer-percentage
+# sn-group-commander-selection-method
+# sn-group-form-distance
+# sn-group-leader-defense-distance
+# sn-hits-before-alliance-change
+# sn-ignore-attack-group-under-attack
+# sn-initial-attack-delay
+# sn-initial-attack-delay-type
+# sn-initial-exploration-required
+# sn-intelligent-gathering
+# sn-max-build-plan-gatherer-percentage
+# sn-maximum-attack-group-size
+# sn-maximum-boat-attack-group-size
+# sn-maximum-boat-defend-group-size
+# sn-maximum-boat-explore-group-size
+# sn-maximum-defend-group-size
+# sn-maximum-explore-group-size
+# sn-maximum-fish-boat-drop-distance
+# sn-maximum-town-size
+# sn-minimum-attack-group-size
+# sn-minimum-dropsite-buffer
+# sn-minimum-explore-group-size
+# sn-minimum-town-size
+# sn-number-attack-groups
+# sn-number-boat-attack-groups
+# sn-number-boat-defend-groups
+# sn-number-boat-explore-groups
+# sn-number-build-attempts-before-skip
+# sn-number-defend-groups
+# sn-number-max-skip-cycles
+# sn-save-scenario-information
+# sn-scale-maximum-attack-group-size
+# sn-scale-minimum-attack-group-size
+# sn-scaling-frequency
+# sn-special-attack-influence1
+# sn-special-attack-type1
+# sn-special-attack-type2
+# sn-special-attack-type3
+# sn-stone-dropsite-distance
+# sn-stone-gatherer-percentage
+# sn-target-evaluation-distance
+# sn-task-ungrouped-soldiers
+# sn-wood-gatherer-percentage
+# sn-zero-priority-distance
