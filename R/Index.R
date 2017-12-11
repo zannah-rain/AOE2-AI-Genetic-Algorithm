537a5e1c648a39410800163063af184614af8c10
zannah-rain <zoe.pellatt.0@gmail.com>
2017-12-11 01:37:14 +0000

    Added ability to call Index backwards (makes looping easier).

--- R/Index.R
+++ R/Index.R
@@ -1,6 +1,9 @@
-Index <- function(x) {
+Index <- function(x, backwards = FALSE) {
   length <- length(x)
   if (length == 0)
     return(NULL)
+  if (backwards) {
+    return(length:1)
+  }
   1:length
 }
